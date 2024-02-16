da402433251add42594218a90da0c4edd638e1f8
Robert de Bock <robert.debock@adfinis.com>
2024-02-16 09:31:54 +0100

    Simplest I can do.

---
+++ main.tf
@@ -0,0 +1,4 @@
+resource "random_pet" "default" {
+  length = 2
+  prefix = "my"
+}
---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "pet_name" {
+  description = "Generated pet name."
+  value       = random_pet.default.id
+}
+
